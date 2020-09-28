05535ead7e50b5ff679a6eb6653c0f7600d25da2
spoiledragon <karlo.gallegos2031@alumnos.udg.mx>
2020-09-28 13:00:44 -0500

    verifica el string y si coincide lanza el tamaño del tipo de dato primitivo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string opc;
+    cin>>opc;
+    //char, int, short, float, double, long
+    if(opc=="char")
+        cout<<sizeof(char)<<endl;
+    if(opc=="int")
+        cout<<sizeof(int)<<endl;
+    if(opc=="short")
+        cout<<sizeof(short)<<endl;
+    if(opc=="float")
+        cout<<sizeof(float)<<endl;
+    if(opc=="double")
+        cout<<sizeof(double)<<endl;
+    if(opc=="long")
+        cout<<sizeof(long)<<endl;
+    
 
     return 0;
 }
